232d7021323b6d0685b723b053f36734fdbc7e06
Lancov_Lavrentiy <stud204036@vyatsu.ru>
2025-06-08 16:46:35 +0300

    Выполнена проверка. Добавлен файл с main

--- WeatherService.cpp
+++ WeatherService.cpp
@@ -2,19 +2,18 @@
 #include"JsonService.h"
 #include"Service.h"
 #include <iostream>
-
+#include"XmlService.h"
 int main() {
     setlocale(LC_ALL,"Russian");
     try {
+        XmlService xs;
+        Weather s = xs.getWeather("weather.xml");
+
         JsonService js;
         Weather w = js.getWeather("weather.json");
-
-        // Выводим результат
         w.print();
     }
     catch (const std::exception& e) {
         std::cerr << "Ошибка: " << e.what() << std::endl;
     }
-
-    return 0;
 }
